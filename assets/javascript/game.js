69f4c6ebc6208827d65751107bb7398f2fd8c02c
rst_979 <richard.taing@gmail.com>
2019-07-10 21:26:39 +1000

    added 4 images, psuedo code html and .js of steps and added jquery link

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Crystal Variables
+// clicks on the crystals
+// Game start and restarts
+// Target Score between 19 and 120
+// Crystal values between 1 and 12
+
+// User function Won/Lost/Reset
+
+// Scores (Current and Target)
+// HTML to display scores
+// Wins and Losses
+// Win Counter
+// Win Count display HTML
+// Loss Counter
+// Loss Count display HTML
+// Current score counter
+// Current score dispaly HTML
+// Reset the Current Score
+// Restart the game
